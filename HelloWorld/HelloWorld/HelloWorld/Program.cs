b02a248e6dcd6e01ff064bd5bc664e1ea3d9122c
RaymondKupBreen <58034773+RaymondKupBreen@users.noreply.github.com>
2020-01-06 15:07:35 +0100

    HelloWorld aangemaakt

---
+++ HelloWorld/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            WriteSomething("hallo");
+        }
+
+        public static void WriteSomething(string writeThis)
+        {
+            Console.WriteLine(writeThis);
+            Console.Read();
+        }
+    }
+}
