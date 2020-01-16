8faeff2fc2cf01e3c86d4f7ec8f10e2c89247562
RaymondKupBreen <58034773+RaymondKupBreen@users.noreply.github.com>
2020-01-16 15:14:01 +0100

    Feestje gemaakt

---
+++ Feestje/Feestje/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Feestje
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbAlcohol = new System.Windows.Forms.CheckBox();
+            this.cbFancy = new System.Windows.Forms.CheckBox();
+            this.nudPeople = new System.Windows.Forms.NumericUpDown();
+            this.lblPeople = new System.Windows.Forms.Label();
+            this.tbTotal = new System.Windows.Forms.TextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPeople)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbAlcohol
+            // 
+            this.cbAlcohol.AutoSize = true;
+            this.cbAlcohol.Location = new System.Drawing.Point(28, 104);
+            this.cbAlcohol.Name = "cbAlcohol";
+            this.cbAlcohol.Size = new System.Drawing.Size(67, 17);
+            this.cbAlcohol.TabIndex = 0;
+            this.cbAlcohol.Text = "Alcohol?";
+            this.cbAlcohol.UseVisualStyleBackColor = true;
+            // 
+            // cbFancy
+            // 
+            this.cbFancy.AutoSize = true;
+            this.cbFancy.Location = new System.Drawing.Point(28, 127);
+            this.cbFancy.Name = "cbFancy";
+            this.cbFancy.Size = new System.Drawing.Size(114, 17);
+            this.cbFancy.TabIndex = 1;
+            this.cbFancy.Text = "Fancy decoration?";
+            this.cbFancy.UseVisualStyleBackColor = true;
+            // 
+            // nudPeople
+            // 
+            this.nudPeople.Location = new System.Drawing.Point(28, 44);
+            this.nudPeople.Name = "nudPeople";
+            this.nudPeople.Size = new System.Drawing.Size(120, 20);
+            this.nudPeople.TabIndex = 2;
+            // 
+            // lblPeople
+            // 
+            this.lblPeople.AutoSize = true;
+            this.lblPeople.Location = new System.Drawing.Point(28, 25);
+            this.lblPeople.Name = "lblPeople";
+            this.lblPeople.Size = new System.Drawing.Size(87, 13);
+            this.lblPeople.TabIndex = 3;
+            this.lblPeople.Text = "Aantal personen:";
+            // 
+            // tbTotal
+            // 
+            this.tbTotal.Enabled = false;
+            this.tbTotal.Location = new System.Drawing.Point(28, 211);
+            this.tbTotal.Name = "tbTotal";
+            this.tbTotal.Size = new System.Drawing.Size(100, 20);
+            this.tbTotal.TabIndex = 4;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(28, 192);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(75, 13);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.Text = "Totale kosten:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(28, 161);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 23);
+            this.btnCalculate.TabIndex = 6;
+            this.btnCalculate.Text = "Bereken kosten";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(275, 309);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.tbTotal);
+            this.Controls.Add(this.lblPeople);
+            this.Controls.Add(this.nudPeople);
+            this.Controls.Add(this.cbFancy);
+            this.Controls.Add(this.cbAlcohol);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudPeople)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox cbAlcohol;
+        private System.Windows.Forms.CheckBox cbFancy;
+        private System.Windows.Forms.NumericUpDown nudPeople;
+        private System.Windows.Forms.Label lblPeople;
+        private System.Windows.Forms.TextBox tbTotal;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ Feestje/Feestje/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Feestje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double total;
+
+            //30 euro standaard decoratie
+            double vasteKosten = 30.00;
+            if (cbFancy.Checked)
+                vasteKosten += 20.00;
+
+            //5 euro pp fris, 25 euro pp voedsel, 7.50 pp standaard decoratie
+            double kostenPerPersoon = 37.50;
+            if (cbAlcohol.Checked)
+                kostenPerPersoon += 15;
+            if (cbFancy.Checked)
+                kostenPerPersoon += 7.50;
+
+            total = vasteKosten + double.Parse(nudPeople.Value.ToString()) * kostenPerPersoon;
+
+            if (!cbAlcohol.Checked)
+                total *= 0.95;
+
+            tbTotal.Text = total.ToString();
+        }
+    }
+}
